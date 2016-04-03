c638d9d7d3a9529006ef08e006fe23781698cc7a
Larry Clem <larryvincentclem@gmail.com>
2016-04-03 17:19:48 -0500

    decidCtrl update

--- public/components/decide/decideCtrl.js
+++ public/components/decide/decideCtrl.js
@@ -1,5 +1,17 @@
 angular.module('optionLab').controller('decideCtrl', function( $scope ){
 
+$scope.decision = {};
 
+$scope.addDecision = function (){
+  decideService.addDecision($scope.decision);
+}
 
-})
+$scope.editDecision = function (){
+  decideService.editDecision($scope.decision);
+}
+
+$scope.deleteDecision = function (){
+  decideService.deleteDecision($scope.decision);
+}
+
+});
